5614b02800a8960375ba91ad20312f376dc43622
Ben Broadley <BenBroadley@driveworks.co.uk>
2021-11-06 16:18:46 +0000

    initial implementation of the calculation method; this will now validate all input constraints.

---
+++ Src/TwoSum/TwoSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TwoSum
+{
+    public class TwoSum
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] Calculate(int[] input, int target)
+        {
+            if (!ValidateInput(input))
+            {
+                throw new ArgumentException("The input parameter does not meet the conditions of the challenge");
+            }
+
+            if (!ValidateTarget(target))
+            {
+                throw new ArgumentException("The taget parameter does not meet the conditions of the challenge");
+            }
+
+            var result = new int[2];
+
+            return result;
+        }
+
+        private static bool ValidateTarget(int target)
+        {
+            return target < -110 || target > 110;
+        }
+
+        private static bool ValidateInput(int[] input)
+        {
+            if (input.Length < 2 || input.Length > 104)
+            {
+                return false;
+            }
+
+            foreach (var inputValue in input)
+            {
+                if (inputValue < -109 || inputValue > 109)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
