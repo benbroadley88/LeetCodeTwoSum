8b627989077aed5abf0439d911a8cd5d03a8bcdf
Ben Broadley <BenBroadley@driveworks.co.uk>
2021-11-07 11:11:46 +0000

    Added TwoSumII unit test project and challenge class outlines - I'm assuming the solution will require something fairly similar to the first

--- Src/TwoSumII/Program.cs
+++ Src/TwoSumII/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace TwoSumII
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<int[]>("--numbers", "The integer array of input numbers."),
+                new Option<int>("--target", "The target sum to find.")
+            };
+
+            rootCommand.Description = "LeetCode TwoSum implementation";
+
+            rootCommand.Handler = CommandHandler.Create<int[], int>((numbers, target) =>
+            {
+                try
+                {
+                    // TODO: Add an implementation
+                    return 0;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return 1;
+                }
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
